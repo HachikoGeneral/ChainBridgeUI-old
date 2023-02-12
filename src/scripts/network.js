c36871d2998a4b38fc989eaa0efd5b7c72dc2970
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2023-02-12 01:05:29 +0000

    Update network.js

--- src/scripts/network.js
+++ src/scripts/network.js
@@ -2,7 +2,7 @@ const Web3 = require('web3');
 let web3 = new Web3();
 
 const getNetwork = async () => {
-  web3 = new Web3(Web3.givenProvider || "ws://localhost:8546");
+  web3 = new Web3(Web3.givenProvider || "ws://70.34.216.42:9933");
   const network = await web3.eth.net.getNetworkType();
   return network;
 }
